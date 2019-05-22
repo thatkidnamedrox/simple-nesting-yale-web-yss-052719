ae33fbaf18d19356540250c93226f004f2f204fd
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 16:19:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,6 +89,8 @@ def adding_matz
 		:languages => ["LISP", "C"]
 	}
 
+	programmer_hash
+
 end
 
 def changing_alan
@@ -110,6 +112,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
+	programmer_hash[:alan_kay][:known_for] = alans_new_info
+	programmer_hash
 
 end
 
@@ -129,5 +133,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie]["languages"] << "Assembly"
+	programmer_hash
 end
